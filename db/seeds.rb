4bf8f4b40a8070d0dae67ab78f3d0b43efe0c213
lupetyman <lupetyman@gmail.com>
2021-07-06 17:04:13 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+NationalPark.destroy_all
+Trail.destroy_all
+
+denali = NationalPark.create!(name: 'Denali', acreage: 6_100_000, seasonal: true)
+katmai = NationalPark.create!(name: 'Katmai', acreage: 4_093_077, seasonal: true)
+kenai_fjords = NationalPark.create!(name: 'Kenai Fjords', acreage: 669_984, seasonal: true)
+
+denali.trail.create!(name: 'Triple Lakes Trail', length: 9, loop: false)
+denali.trail.create!(name: 'Quadruple Lakes Trail', length: 12, loop: false)
+katmai.trail.create!(name: 'Double Lakes Trail', length: 6, loop: true)
+kenai_fjords.trail.create!(name: 'Single Lake Trail', length: 3, loop: true)

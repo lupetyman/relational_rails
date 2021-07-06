80a96454db6a3e3f9572641b1e7c21c38e4ee117
lupetyman <lupetyman@gmail.com>
2021-07-06 17:11:40 -0600

    Add #show

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/national_parks', to: 'national_parks#index'
   get '/national_parks/:id', to: "national_parks#show"
   get '/trails', to: 'trails#index'
+  get '/trails/:id', to: 'trails#show'
 end

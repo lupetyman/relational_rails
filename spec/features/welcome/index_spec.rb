5429d522936e3c44cd6a54415d2c65f5ae8e4bdf
lupetyman <lupetyman@gmail.com>
2021-07-07 16:52:47 -0600

    Update welcome page for resource links and testing

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -6,4 +6,36 @@ RSpec.describe 'welcome index page' do
 
     expect(page).to have_content("Yay! You're on Relational Rails")
   end
+
+  it 'can link to national parks page' do
+    visit '/'
+
+    click_link 'National Parks'
+
+    expect(current_path).to eq('/national_parks')
+  end
+
+  it 'can link to trails page' do
+    visit '/'
+
+    click_link 'Trails'
+
+    expect(current_path).to eq('/trails')
+  end
+
+  it 'can link to campgrounds page' do
+    visit '/'
+
+    click_link 'Campgrounds'
+
+    expect(current_path).to eq('/campgrounds')
+  end
+
+  it 'can link to campsites page' do
+    visit '/'
+    
+    click_link 'Campsites'
+
+    expect(current_path).to eq('/campsites')
+  end
 end

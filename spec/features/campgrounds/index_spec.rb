442514ff6c828fb3c4dffb26d3bf4b62b26980a7
lupetyman <lupetyman@gmail.com>
2021-07-12 15:27:30 -0600

    Update quotes

--- spec/features/campgrounds/index_spec.rb
+++ spec/features/campgrounds/index_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'campgrounds index page' do
   before :each do
-    @sunset_point = Campground.create!(name: "Sunset Point Campground", reservation_allowed: false, max_nights: 7)
-    @cherry_creek = Campground.create!(name: "Cherry Creek Campground", reservation_allowed: true, max_nights: 14)
-    @chatfield = Campground.create!(name: "Chatfield Campground", reservation_allowed: true, max_nights: 14)
+    @sunset_point = Campground.create!(name: 'Sunset Point Campground', reservation_allowed: false, max_nights: 7)
+    @cherry_creek = Campground.create!(name: 'Cherry Creek Campground', reservation_allowed: true, max_nights: 14)
+    @chatfield = Campground.create!(name: 'Chatfield Campground', reservation_allowed: true, max_nights: 14)
   end
 
   it 'shows all the campgrounds and their names' do

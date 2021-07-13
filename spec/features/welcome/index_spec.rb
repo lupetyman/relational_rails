9d99275e68cd51551139676efd369942ab4aff76
lupetyman <lupetyman@gmail.com>
2021-07-12 20:35:57 -0600

    Remove spacing in tests

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -9,33 +9,25 @@ RSpec.describe 'welcome index page' do
 
   it 'can link to national parks page' do
     visit '/'
-
     click_link 'National Parks'
-
     expect(current_path).to eq('/national_parks')
   end
 
   it 'can link to trails page' do
     visit '/'
-
     click_link 'Trails'
-
     expect(current_path).to eq('/trails')
   end
 
   it 'can link to campgrounds page' do
     visit '/'
-
     click_link 'Campgrounds'
-
     expect(current_path).to eq('/campgrounds')
   end
 
   it 'can link to campsites page' do
     visit '/'
-
     click_link 'Campsites'
-
     expect(current_path).to eq('/campsites')
   end
 

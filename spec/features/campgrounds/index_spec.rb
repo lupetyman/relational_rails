88b1047e79f2a73b5f945b2ed6ef3e2e7a7cf40b
lupetyman <lupetyman@gmail.com>
2021-07-13 19:39:48 -0600

    Test order campground by campsite count

--- spec/features/campgrounds/index_spec.rb
+++ spec/features/campgrounds/index_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'campgrounds index page' do
     @sunset_point = Campground.create!(name: 'Sunset Point Campground', reservation_allowed: false, max_nights: 7)
     @cherry_creek = Campground.create!(name: 'Cherry Creek Campground', reservation_allowed: true, max_nights: 14)
     @chatfield = Campground.create!(name: 'Chatfield Campground', reservation_allowed: true, max_nights: 14)
+    @site_15 = @sunset_point.campsites.create!(name: 'Site 15', tent_only: true, overnight_fee: 26)
+    @cottonwood_111= @cherry_creek.campsites.create!(name: 'Cottonwood Grove Site 111', tent_only: false, overnight_fee: 28)
+    @abilene_10= @cherry_creek.campsites.create!(name: 'Abilene Site 10', tent_only: false, overnight_fee: 41)
   end
 
   it 'shows all the campgrounds and their names' do
@@ -61,5 +64,18 @@ RSpec.describe 'campgrounds index page' do
     visit '/campgrounds'
     click_link 'Trail Index'
     expect(current_path).to eq('/trails')
+  end
+
+  it 'can sort by the number of campsites they have' do
+    visit '/campgrounds'
+
+    click_link 'Sort by Number of Campsites'
+    
+    expect(current_path).to eq('/campgrounds')
+    expect(@cherry_creek.name).to appear_before(@sunset_point.name)
+    expect(@sunset_point.name).to appear_before(@chatfield.name)
+    expect(page).to have_content(@sunset_point.campsite_count)
+    expect(page).to have_content(@cherry_creek.campsite_count)
+    expect(page).to have_content(@chatfield.campsite_count)
   end
 end

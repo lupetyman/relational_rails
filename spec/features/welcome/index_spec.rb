66bcccac15ec5a61dcf933b62ba9780dd944f762
lupetyman <lupetyman@gmail.com>
2021-07-14 19:24:06 -0600

    Remove duplicate tests

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -9,43 +9,25 @@ RSpec.describe 'welcome index page' do
 
   it 'can link to national parks page' do
     visit '/'
-    click_link 'National Parks'
+    click_link 'National Park Index'
     expect(current_path).to eq('/national_parks')
   end
 
   it 'can link to trails page' do
     visit '/'
-    click_link 'Trails'
+    click_link 'Trail Index'
     expect(current_path).to eq('/trails')
   end
 
   it 'can link to campgrounds page' do
     visit '/'
-    click_link 'Campgrounds'
+    click_link 'Campground Index'
     expect(current_path).to eq('/campgrounds')
   end
 
   it 'can link to campsites page' do
     visit '/'
-    click_link 'Campsites'
-    expect(current_path).to eq('/campsites')
-  end
-
-  it 'can link to other index pages' do
-    visit '/'
-    click_link 'Campground Index'
-    expect(current_path).to eq('/campgrounds')
-
-    visit '/'
     click_link 'Campsite Index'
     expect(current_path).to eq('/campsites')
-
-    visit '/'
-    click_link 'National Park Index'
-    expect(current_path).to eq('/national_parks')
-
-    visit '/'
-    click_link 'Trail Index'
-    expect(current_path).to eq('/trails')
   end
 end
